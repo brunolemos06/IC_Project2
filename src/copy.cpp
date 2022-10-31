a8965e94f896e3611b6230b721302d98b9f92df6
joaoantonioViegas <joao.antonio.viegas@ua.pt>
2022-10-31 18:31:25 +0000

    copy.cpp

---
+++ src/copy.cpp
@@ -0,0 +1,58 @@
+
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+//Using the OpenCV library, implement a program to copy an image, pixel by pixel, from one file
+//to another file. Both file names should be passed as command line arguments to the program.
+
+int main(int argc, char** argv )
+{
+    if ( argc != 3 )
+    {
+        printf("usage: DisplayImage.out <Image_Path> <Image_Path>\n");
+        return -1;
+    }
+
+    Mat image;
+    image = imread( argv[1], 1 );
+
+    if ( !image.data )
+    {
+        printf("No image data \n");
+        return -1;
+    }
+
+    Mat image2;
+    image2 = imread( argv[2], 1 );
+
+    if ( !image2.data )
+    {
+        printf("No image data2 \n");
+        return -1;
+    }
+
+    Mat image3(image.rows, image.cols, CV_8UC3, Scalar(0,0,0));
+
+    for(int i = 0; i < image.rows; i++)
+    {
+        for(int j = 0; j < image.cols; j++)
+        {
+            image3.at<Vec3b>(i,j)[0] = image.at<Vec3b>(i,j)[0];
+            image3.at<Vec3b>(i,j)[1] = image.at<Vec3b>(i,j)[1];
+            image3.at<Vec3b>(i,j)[2] = image.at<Vec3b>(i,j)[2];
+        }
+    }
+
+    //write the image to the second file
+    imwrite(argv[2], image3);
+    namedWindow("Display Image", WINDOW_AUTOSIZE );
+    imshow("Display Image", image3);
+
+    waitKey(0);
+
+    return 0;
+}
