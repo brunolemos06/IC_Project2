3513ce5695be7bef694143885b48c7c06269ed9b
blemos <blemos@ua.pt>
2022-10-31 17:36:55 +0000

    makefile

---
+++ src/ex2_a.cpp
@@ -0,0 +1,9 @@
+
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <stdio.h>
+using namespace std; // std dont need
+int main(){
+    cout << "Hello World!" << endl;
+    cout << "Hello oi!" << endl;
+}
