574a57f7d9c8cdd60479d56b08a7b81fc8d3f27c
tiago <tamarques@ua.pt>
2022-11-09 22:30:40 +0000

    fully functional golomb code with test program

--- src/golomb.h
+++ src/golomb.h
@@ -15,34 +15,52 @@ class golomb{
         int m;
         int power_of_2;
         map<string, int> decode_bin;
+        map<int, string> encode_bin;
 
     
     public:
         golomb(int m){
             this->m = m;
             this->power_of_2 = (ceil(log2(m)) == floor(log2(m)));
-            map<string, int> table;
+            map<string, int> decode_table;
+            map<int, string> encode_table;
+            int b = ceil(log2(m));
             if(!this->power_of_2){
-                int b = ceil(log2(m));
                 for(int i=0; i<(pow(2,b)-m); i++){
                     //create bin string with b-1 bits
-                    string bin = bitset<32>(i).to_string();
-                    bin = bin.substr(8-b+1, b-1);
-                    table[bin] = i;
+                    string bin;
+                    copy_n(bitset<32>(i).to_string().end()-b, b, back_inserter(bin));
+                    decode_table[bin] = i;
+                    encode_table[i] = bin;
                 }
                 //code remaining values of r by coding the number r+(2^b - m) with b bits
                 for(int i=(pow(2,b)-m); i<m; i++){
                     int n = i + (pow(2,b) - m);
                     //create bin string with b bits
-                    string bin = bitset<32>(n).to_string();
-                    bin = bin.substr(8-b, b);
-                    table[bin] = i;
+                    string bin;
+                    copy_n(bitset<32>(n).to_string().end()-b, b, back_inserter(bin));
+                    decode_table[bin] = i;
+                    encode_table[i] = bin;
                 }
             }else{
-                map<string, int> table;
                 //create bin string with b bits
+                for(int i=0; i<m; i++){
+                    string bin;
+                    copy_n(bitset<32>(i).to_string().end()-b, b, back_inserter(bin));
+                    decode_table[bin] = i;
+                    encode_table[i] = bin;
+                }
             }
-            this->decode_bin = table;
+            this->decode_bin = decode_table;
+            this->encode_bin = encode_table;
+            //print tables
+            // cout << "DECODE TABLE" << endl;
+            // for (auto const& x : this->decode_bin)
+            //     cout << x.first << " " << x.second << endl;
+            // cout << "ENCODE TABLE" << endl;
+            // for (auto const& x : this->encode_bin)
+            //     cout << x.first << " " << x.second << endl;
+
         }
 
         //decode binary string to decimal
@@ -58,36 +76,41 @@ class golomb{
             }
             //convert the rest of the bits to decimal
             string rest = bits.substr(count+1);
-            int decimal = stoi(rest, 0, 2);
+            int decimal = decode_bin[rest];
+            //sum unary and binary
+            int result = count*m + decimal; 
             //if the number is even, make it negative
-            if(decimal % 2 == 0){
-                return -(((count*m) + decimal)/2);
-            }
-            
-            return (((count*m) + decimal)-1)/2; 
+            if(result % 2 == 0){
+                return -(result/2);
+            }else{
+                return (result-1)/2;
+            } 
         }
 
         //encode decimal to golomb code string
         string encode_number(int n){
             //map n<=0 to positive even numbers and n>0 to positive odd numbers
+            cout << "\tORIGINAL: " << n << endl;
             if(n <= 0){
                 n = -2*n;
             }else{
                 n = 2*n + 1;
             }
             //calculate unary part
-            string unary = calculate_unary(n);
+            cout << "\tMAPPED: " << n << endl;
+            string unary = encode_unary(n);
             //calculate binary part
-            string binary = calculate_binary(n);
+            string binary = encode_decimal(n);
             //concatenate unary and binary
             return unary + binary;
         }
 
         //receives natural number and returns the binary string of unary part
-        string calculate_unary(int n){
+        string encode_unary(int n){
             //calculate unary part
             int f = n/m;
             //create unary string
+            cout << "\t\tF: " << f <<"  ";
             string unary = "";
             //write 1 f times
             for(int i = 0; i < f; i++){
@@ -95,36 +118,17 @@ class golomb{
             }
             //add separator aka "0"
             unary += "0";
+            cout << "UNARY: " << unary << endl;
             return unary;
         }
 
-        //receives natural number and returns the binary string of binary part
-        string calculate_binary(int n){
+        //receives natural number and returns the binary string of decimal part
+        string encode_decimal(int n){
             //calculate decimal part
             int r = n%m;
-            //create binary string
-            string binary;
-            if(!this->power_of_2){
-                //calculate b
-                int b = ceil(log2(m));
-                //if r is less than 2^b - m, code r with b-1 bits
-                if(r < (pow(2,b) - m)){
-                    //create bin string with b-1 bits
-                    binary = bitset<32>(r).to_string();
-                    binary = binary.substr(8-b+1, b-1);
-                //else code r+(2^b - m) with b bits
-                }else{
-                    //create bin string with b bits
-                    binary = bitset<32>(r+(pow(2,b) - m)).to_string();
-                    binary = binary.substr(8-b, b);
-                }
-            }else{
-                //calculate b
-                int b = log2(m);
-                binary = bitset<32>(r).to_string();
-                binary = binary.substr(8-b, b);
-            }
-            return binary;
+            cout << "\t\tR: " << r << "  ";
+            cout << "BIN: " << this->encode_bin[r] << endl;
+            return this->encode_bin[r];
         }
 };
 
