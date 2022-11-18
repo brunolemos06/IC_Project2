4c5787914a26b63c19a6665a791b1f00b117a753
joaoantonioViegas <joao.antonio.viegas@ua.pt>
2022-11-18 15:31:17 +0000

    codifing and decodifing

--- src/secgolomb.cpp
+++ src/secgolomb.cpp
@@ -14,7 +14,24 @@ int main(int argc, char** argv ){
 
     secgolomb secgolomb(i,m);
     
-    cout << secgolomb.encode(37000,4)<< endl;
+    //read the file encode.txt
+    FILE *filer;
+    filer = fopen(argv[1],"r");
+    FILE *filew;
+    filew = fopen(argv[2],"w");
+    //print the size of the file
+    fseek(filer,0,SEEK_END);
+    int size = ftell(filer);
+    fseek(filer,0,SEEK_SET);
+    //create an array with the size of the file
+    int *array = new int[size];
+    //read each int of the file 
+    while(fscanf(filer,"%d",&i) != EOF){
+        //set the array with the int read
+        for(int j = 0; j < size; j++){
+            array[j] = i;
+        }
+    }
     
     return 0;
 }
--- src/secgolomb.h
+++ src/secgolomb.h
@@ -7,6 +7,8 @@
 #include <stdlib.h>
 #include <bitset>
 #include <tgmath.h> 
+#include <map>
+#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -103,5 +105,154 @@ class secgolomb{
             string binary = decimal_to_binary(r,2);
             return binary;
         }
+        map<string, int> calcdecode(int m){
+            map<string, int> decode_table;
+            int b = ceil(log2(m));
+            if(!(ceil(log2(m)) == floor(log2(m)))){
+                // cout << "NOT power of 2" << endl;
+                for(int i=0; i<(pow(2,b)-m); i++){
+                    //create bin string with b-1 bits
+                    string bin;
+                    copy_n(bitset<32>(i).to_string().end()-b+1, b-1, back_inserter(bin));
+                    decode_table[bin] = i;
+                    // cout << "decode_table[" << bin << "] = " << i << endl;
+                    //store the value in the map
+                }
+                //code remaining values of r by coding the number r+(2^b - m) with b bits
+                for(int i=(pow(2,b)-m); i<m; i++){
+                    int n = i + (pow(2,b) - m);
+                    //create bin string with b bits
+                    string bin;
+                    copy_n(bitset<32>(n).to_string().end()-b, b, back_inserter(bin));
+                    decode_table[bin] = i;
+                    // cout << "decode_table[" << bin << "] = " << i << endl;
+                    // cout << "encode_table[" << i << "] = " << bin << endl;
+                    //store the value in the map
+                }
+            }else{
+                //create bin string with b bits
+                for(int i=0; i<m; i++){
+                    string bin;
+                    copy_n(bitset<32>(i).to_string().end()-b, b, back_inserter(bin));
+                    decode_table[bin] = i;
+                    // cout << "decode_table[" << bin << "] = " << i << endl;
+                }
+            }
+            return decode_table;
+        }
+        
+
+        void decoder(string code, int m){
+            //read the code until the first 0
+            int q = 0;
+            int r = 0;
+            int b = ceil(log2(m));
+            int aux = pow(2,b) - m;
+            int i = 0;
+            int decoded = 0;
+            string r_string;
+            map<string, int> map;
+            for (int i = 0; i < code.length(); i++){
+                //count the firts ones before the first 0
+                if(code[i] == '1'){
+                    q++;
+                }else{
+                    break;
+                }                         
+            }
+            for (int j = q+1; j < code.length()-1; j++)
+            {
+                //store the value of r in a string
+                r_string += code[j];
+            }
+            // cout << "q: " << q << endl;
+            // cout << r_string <<endl;
+            map = calcdecode(m);
+            for(auto it = map.begin(); it != map.end(); ++it){
+            // cout << it->first << " => " << it->second << '\n';
+            //find the value of r_string in the map
+                if(it->first == r_string){
+                    r = it->second;
+                }
+            }
+            // cout << "r: " << r << endl;
+            decoded = (q*m)+r;
+            if(code[code.length()-1]=='1'){
+                decoded = decoded * -1;
+            }
+            cout << "decoder: " << decoded << endl;
+        }
+
+        string separator(string code,int m){
+            map<string, int> map;
+            map = calcdecode(5);  
+            string aux;
+            // for(auto it = map.begin(); it != map.end(); ++it){
+            //     cout << it->first << " => " << it->second << '\n';
+            // }
+            int max = map.size();
+            int findzero = 0;
+            string aux2;
+            string aux3;
+            int aux4;
+            string r;
+            int binfound=0;
+            string signal;
+            int signalfound=0;
+            string words;
+            string word;
+            int inc=0;
+            for (int i=0; i<code.length(); i++){
+                while (findzero==0){
+                    aux2 += code[i];
+                    if(code[i]=='1'){
+                        inc++;
+                    }
+                    if(code[i]=='0'){
+                        findzero = 1;
+                        r+=1;   
+                        // cout<<"unario: "<<aux2<<endl;   
+                    }   
+                    i++;
+                }
+                if(findzero==1){
+                    aux3 += code[i];
+                    for(auto it = map.begin(); it != map.end(); ++it){
+                        if(it->first == aux3){
+                            aux4 = it->second;
+                            r=it->first;
+                            // cout<<"binario: "<< r <<endl;
+                            binfound=1;
+                            i++;
+                        }
+                    }
+                }
+                if(binfound==1){
+                    signal=code[i];
+                    if(signal=="1"){
+                        // cout<<"sinal: "<<signal<<endl;
+                        signalfound=1;
+                    }else{
+                        // cout<<"sinal: "<<signal<<endl;
+                        signalfound=1;
+                    }
+                }
+                if(binfound==1&&findzero==1&&signalfound==1){
+                    word=aux2+r+signal;
+                    decoder(word,m);
+                    aux2="";
+                    aux3="";
+                    aux4=0;
+                    inc=0;
+                    findzero=0;
+                    binfound=0;
+                    signalfound=0;
+                }
+                
+            }
+            
+            return "moca";
+        }
+            
         
 };
